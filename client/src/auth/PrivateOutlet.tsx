7d6f1612cc508e48ad9c666361a1ff897ded3f19
d_flex <hiyoo95@gmail.com>
2022-03-05 20:39:16 +0900

    프론트엔드 반응형 레이아웃 반영 중/ 레시피 상세 페이지 레이아웃 변경

--- client/src/auth/PrivateOutlet.tsx
+++ client/src/auth/PrivateOutlet.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
-import { authAtom } from '../store/store';
+import { authState } from '../store/store';
 import { useRecoilValue } from 'recoil';
-import Cookies from 'universal-cookie';
 
 const PrivateOutlet: React.FC = () => {
-  const authenticated = useRecoilValue(authAtom);
-  const cookie = new Cookies();
+  const authenticated = useRecoilValue(authState);
+
   return authenticated ? <Outlet /> : <Navigate to='/' />;
 };
 
